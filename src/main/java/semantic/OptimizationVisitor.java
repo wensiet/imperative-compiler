8e34181ada7fb34e0139bbeeed0a0f60bffff0a9
Saveliy Lekhtin <s.lekhtin@innopolis.university>
2024-10-28 20:04:44 +0300

    feat: draft for optimization

--- src/main/java/semantic/OptimizationVisitor.java
+++ src/main/java/semantic/OptimizationVisitor.java
@@ -6,145 +6,111 @@ import org.antlr.v4.runtime.tree.ParseTree;
 import org.antlr.v4.runtime.tree.TerminalNodeImpl;
 import org.antlr.v4.runtime.CommonToken;
 
+import java.util.Objects;
+
 public class OptimizationVisitor extends ImperativeCompConstBaseVisitor<Object> {
-    @Override
-    public Object visitAssignment(ImperativeCompConstParser.AssignmentContext ctx) {
-        System.out.println("sjdadd");
-        Object value = visit(ctx.expression());
-        if (value instanceof Integer || value instanceof Double || value instanceof Boolean) {
-            ctx.expression().getParent().removeLastChild();
-            ctx.expression().getParent().addChild(new TerminalNodeImpl(new CommonToken(getTokenType(value), value.toString())));
-        }
-        return null;
-
-        /*
-        (input (program (simple_declaration (variable_declaration var a : (type (primitive_type integer)) is (expression (relation (simple (factor (summand (primary 1)) + (summand (primary 1)))))) ;))) <EOF>)
-
-        (input (program (simple_declaration (variable_declaration var a : (type (primitive_type integer)) is (expression (relation (simple (factor (summand (primary 2)))))) ;))) <EOF>)
-
-         */
-    }
-
-    @Override
-    public Object visitSimple_declaration(ImperativeCompConstParser.Simple_declarationContext ctx) {
-        System.out.println("simple declaration");
-        System.out.println(ctx.getText());
-        System.out.println(ctx.getChildCount());
-        return visitChildren(ctx);
-    }
+//    @Override
+//    public Object visitAssignment(ImperativeCompConstParser.AssignmentContext ctx) {
+//        System.out.println("visit assignment");
+//        Object value = visit(ctx.expression());
+//        if (value instanceof Integer || value instanceof Double || value instanceof Boolean) {
+//            ctx.expression().getParent().removeLastChild();
+//            ctx.expression().getParent().addChild(new TerminalNodeImpl(new CommonToken(getTokenType(value), value.toString())));
+//        }
+//        return null;
+//
+//        /*
+//        (input (program (simple_declaration (variable_declaration var a : (type (primitive_type integer)) is (expression (relation (simple (factor (summand (primary 1)) + (summand (primary 1)))))) ;))) <EOF>)
+//
+//        (input (program (simple_declaration (variable_declaration var a : (type (primitive_type integer)) is (expression (relation (simple (factor (summand (primary 2)))))) ;))) <EOF>)
+//
+//         */
+//    }
 
 //    @Override
-//    public Object visitRelation(ImperativeCompConstParser.RelationContext ctx) {
+//    public Object visitSimple_declaration(ImperativeCompConstParser.Simple_declarationContext ctx) {
+//        System.out.println("simple declaration");
+//        System.out.println(ctx.getText());
 //        System.out.println(ctx.getChildCount());
 //        return visitChildren(ctx);
 //    }
 
 
+//    @Override
+//    public Object visitSimple(ImperativeCompConstParser.SimpleContext ctx) {
+//        System.out.println(ctx.getChildCount());
+//        System.out.println(ctx.getChild(0).getText());
+//        return visitChildren(ctx);
+//    }
+
     @Override
-    public Object visitExpression(ImperativeCompConstParser.ExpressionContext ctx) {
+    public Object visitFactor(ImperativeCompConstParser.FactorContext ctx) {
+
         if (ctx.getChildCount() == 3) {
-            Object left = visit(ctx.relation(0));
-            Object right = visit(ctx.relation(1));
-            if (left instanceof Integer && right instanceof Integer) {
-                int result = 0;
-                String operator = ctx.getChild(1).getText();
-                switch (operator) {
-                    case "+":
-                        result = (Integer) left + (Integer) right;
-                        break;
-                    case "-":
-                        result = (Integer) left - (Integer) right;
-                        break;
-                    case "*":
-                        result = (Integer) left * (Integer) right;
-                        break;
-                    case "/":
-                        if ((Integer) right != 0) {
-                            result = (Integer) left / (Integer) right;
-                        } else {
-                            throw new ArithmeticException("Division by zero");
-                        }
-                        break;
-                    case "<":
-                        result = ((Integer) left < (Integer) right) ? 1 : 0;
-                        break;
-                    case ">":
-                        result = ((Integer) left > (Integer) right) ? 1 : 0;
-                        break;
-                    case "=":
-                        result = ((Integer) left == (Integer) right) ? 1 : 0;
-                        break;
+            ParseTree left = ctx.getChild(0);
+            ParseTree operator = ctx.getChild(1);
+            ParseTree right = ctx.getChild(2);
+            /*
+            (input (program (simple_declaration (variable_declaration var a : (type (primitive_type integer)) is (expression (relation (simple (factor (summand (primary 1)) + (summand (primary 1)))))) ;))) <EOF>)
+
+
+            (input (program (simple_declaration (variable_declaration var a : (type (primitive_type integer)) is (expression (relation (simple (factor (summand (primary 2)))))) ;))) <EOF>)
+             */
+
+            if (left instanceof ImperativeCompConstParser.SummandContext && right instanceof ImperativeCompConstParser.SummandContext) {
+                ImperativeCompConstParser.PrimaryContext leftPrimary = ((ImperativeCompConstParser.SummandContext) left).primary();
+                ImperativeCompConstParser.PrimaryContext rightPrimary = ((ImperativeCompConstParser.SummandContext) right).primary();
+
+                if (isIntOrReal(leftPrimary) && isIntOrReal(rightPrimary) && Objects.equals(leftPrimary.getText(), rightPrimary.getText())) {
+                    int leftValue = Integer.parseInt(leftPrimary.INT().getText());
+                    int rightValue = Integer.parseInt(rightPrimary.INT().getText());
+                    int sum = leftValue + rightValue;
+
+                    // Create a new PrimaryContext with the sum as an integer literal
+                    ImperativeCompConstParser.PrimaryContext newPrimary = new ImperativeCompConstParser.PrimaryContext(ctx, ctx.invokingState);
+                    TerminalNodeImpl newIntNode = new TerminalNodeImpl(new CommonToken(ImperativeCompConstParser.INT, Integer.toString(sum)));
+                    newPrimary.addChild(newIntNode);
+
+                    // Clear all children of FactorContext and add the new PrimaryContext directly
+                    while (ctx.getChildCount() > 0) {
+                        ctx.removeLastChild();
+                    }
+                    ctx.addAnyChild(newPrimary);
+
+                    return null;
                 }
-                // Replace the expression with the simplified constant
-                ctx.getParent().removeLastChild();
-                ctx.getParent().addChild(new TerminalNodeImpl(new CommonToken(getTokenType(result), Integer.toString(result))));
-                return result;
-            } else if (left instanceof Double && right instanceof Double) {
-                double result = 0;
-                String operator = ctx.getChild(1).getText();
-                switch (operator) {
-                    case "+":
-                        result = (Double) left + (Double) right;
-                        break;
-                    case "-":
-                        result = (Double) left - (Double) right;
-                        break;
-                    case "*":
-                        result = (Double) left * (Double) right;
-                        break;
-                    case "/":
-                        if ((Double) right != 0) {
-                            result = (Double) left / (Double) right;
-                        } else {
-                            throw new ArithmeticException("Division by zero");
-                        }
-                        break;
-                    case "<":
-                        result = ((Double) left < (Double) right) ? 1 : 0;
-                        break;
-                    case ">":
-                        result = ((Double) left > (Double) right) ? 1 : 0;
-                        break;
-                    case "=":
-                        result = ((Double) left == (Double) right) ? 1 : 0;
-                        break;
-                }
-                // Replace the expression with the simplified constant
-                ctx.getParent().removeLastChild();
-                ctx.getParent().addChild(new TerminalNodeImpl(new CommonToken(getTokenType(result), Double.toString(result))));
-                return result;
-            } else if (left instanceof Boolean && right instanceof Boolean) {
-                boolean result = false;
-                String operator = ctx.getChild(1).getText();
-                switch (operator) {
-                    case "and":
-                        result = (Boolean) left && (Boolean) right;
-                        break;
-                    case "or":
-                        result = (Boolean) left || (Boolean) right;
-                        break;
-                    case "xor":
-                        result = (Boolean) left ^ (Boolean) right;
-                        break;
-                }
-                // Replace the expression with the simplified constant
-                ctx.getParent().removeLastChild();
-                ctx.getParent().addChild(new TerminalNodeImpl(new CommonToken(getTokenType(result), Boolean.toString(result))));
-                return result;
-            }
-        } else if (ctx.getChildCount() == 1) { // Terminal node (NUMBER, REAL, TRUE, FALSE)
-            if (ctx.getChild(0).getText().equals("true")) {
-                return true;
-            } else if (ctx.getChild(0).getText().equals("false")) {
-                return false;
-            } else if (ctx.getChild(0).getText().matches("[0-9]+")) {
-                return Integer.parseInt(ctx.getChild(0).getText());
-            } else if (ctx.getChild(0).getText().matches("[0-9]+[.][0-9]+")) {
-                return Double.parseDouble(ctx.getChild(0).getText());
             }
         }
-        return null;
+        return visitChildren(ctx);
     }
+
+    private boolean isIntOrReal(ImperativeCompConstParser.PrimaryContext primary) {
+        return primary.INT() != null || primary.REAL() != null;
+    }
+
+
+//    @Override
+//    public Object visitRelation(ImperativeCompConstParser.RelationContext ctx) {
+//        System.out.println("visit relation");
+//        System.out.println(ctx.getChildCount());
+//        System.out.println(ctx.getChild(0).getText());
+//        return visitChildren(ctx);
+//    }
+
+//    @Override
+//    public Object visitFactor(ImperativeCompConstParser.FactorContext ctx) {
+//        System.out.println("visit factor");
+//        System.out.println(ctx.getChildCount());
+//        System.out.println(ctx.getChild(0).getText());
+//        return visitChildren(ctx);
+//    }
+
+//    @Override
+//    public Object visitExpression(ImperativeCompConstParser.ExpressionContext ctx) {
+//        System.out.println("visit expression");
+//        return visitChildren(ctx);
+//    }
+
 
     private int getTokenType(Object value) {
         if (value instanceof Integer) {
