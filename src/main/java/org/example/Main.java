7b64bc6333d0e9845ebfd79eaf21cee061ccff0c
Iskander Shamsutdinov <wensietyt@gmail.com>
2024-11-18 21:00:05 +0300

    feat(records): add jasmin

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,16 +1,13 @@
 package org.example;
 
-
 import org.example.tester.RuntimeTestCaseParams;
 import org.example.tester.TestCase;
-import org.example.tester.TestLoader;
 import org.example.tester.TestRunner;
 
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.List;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
@@ -29,18 +26,18 @@ public class Main {
     private static void runDebugTest() throws InterruptedException, IOException {
         String input = readFile(new File("debug"));
         TestRunner runner = new TestRunner();
-        runner.run(new TestCase("debug", input, new RuntimeTestCaseParams(0, "1")));
+        runner.runUnsafe(new TestCase("debug", input, new RuntimeTestCaseParams(0, "1")));
     }
 
     public static void main(String[] args) throws InterruptedException {
         try {
-            TestRunner runner = new TestRunner();
-            List<TestCase> tests = new TestLoader("conftest.yaml").getTestCases();
-            for (TestCase test : tests) {
-                System.out.println("Running test case: " + test.name);
-                runner.run(test);
-            }
-//            runDebugTest();
+//            TestRunner runner = new TestRunner();
+//            List<TestCase> tests = new TestLoader("conftest.yaml").getTestCases();
+//            for (TestCase test : tests) {
+//                System.out.println("Running test case: " + test.name);
+//                runner.run(test);
+//            }
+            runDebugTest();
         } catch (IOException | InterruptedException e) {
             throw new RuntimeException(e);
         }
--- src/main/java/org/example/tester/TestRunner.java
+++ src/main/java/org/example/tester/TestRunner.java
@@ -7,15 +7,12 @@ import jasmin.JasMain;
 import org.antlr.v4.runtime.CharStreams;
 import org.antlr.v4.runtime.CommonTokenStream;
 import semantic.Optimizer;
-import semantic.SemanticAnalyzerVisitor;
-import utils.TreeUtils;
 
 import java.io.BufferedReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 import java.util.Objects;
 
@@ -25,51 +22,54 @@ public class TestRunner {
     public static final String ANSI_RED = "\u001B[31m";
     public static final String ANSI_GREEN = "\u001B[32m";
 
-    public void run(TestCase testCase) throws InterruptedException, IOException {
-        try {
+    public void runUnsafe(TestCase testCase) throws IOException, InterruptedException {
+        // Lexer
+        ImperativeCompConstLexer lexer = new ImperativeCompConstLexer(CharStreams.fromString(testCase.input));
 
-            // Lexer
-            ImperativeCompConstLexer lexer = new ImperativeCompConstLexer(CharStreams.fromString(testCase.input));
+        // Parser
+        ImperativeCompConstParser parser = new ImperativeCompConstParser(new CommonTokenStream(lexer));
+        parser.setErrorHandler(new org.example.tester.ThrowingErrorStrategy());
+        ImperativeCompConstParser.InputContext parseTree = parser.input();
 
-            // Parser
-            ImperativeCompConstParser parser = new ImperativeCompConstParser(new CommonTokenStream(lexer));
-            parser.setErrorHandler(new org.example.tester.ThrowingErrorStrategy());
-            ImperativeCompConstParser.InputContext parseTree = parser.input();
+        // Semantic
+        // SemanticAnalyzerVisitor semanticVisitor = new SemanticAnalyzerVisitor();
+        // semanticVisitor.visit(parseTree);
 
-            // Semantic
-            // SemanticAnalyzerVisitor semanticVisitor = new SemanticAnalyzerVisitor();
-            // semanticVisitor.visit(parseTree);
+        // List<String> ruleNamesList = Arrays.asList(parser.getRuleNames());
+        // String originalTree = TreeUtils.toPrettyTree(parseTree, ruleNamesList);
 
-            // List<String> ruleNamesList = Arrays.asList(parser.getRuleNames());
-            // String originalTree = TreeUtils.toPrettyTree(parseTree, ruleNamesList);
+        // Optimizer
+        Optimizer optimizer = new Optimizer();
+        optimizer.optimize(parseTree);
 
-            // Optimizer
-            Optimizer optimizer = new Optimizer();
-            optimizer.optimize(parseTree);
+        // Compiler
+        List<String> files = new ArrayList<>();
+        CompileVisitor compileVisitor = new CompileVisitor();
+        compileVisitor.visit(parseTree);
+        try (FileWriter fileWriter = new FileWriter("TestRun.j")) {
+            fileWriter.write(compileVisitor.getJasminCode());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        files.add("TestRun.j");
 
-            // Compiler
-            List<String> files = new ArrayList<>();
-            CompileVisitor compileVisitor = new CompileVisitor();
-            compileVisitor.visit(parseTree);
-            try (FileWriter fileWriter = new FileWriter("TestRun.j")) {
-                fileWriter.write(compileVisitor.getJasminCode());
+        for (var record : compileVisitor.getRecords()) {
+            String fileName = record.name + ".j";
+            try (FileWriter fileWriter = new FileWriter(fileName)) {
+                fileWriter.write(record.code);
             } catch (IOException e) {
                 e.printStackTrace();
             }
-            files.add("TestRun.j");
+            files.add(fileName);
+        }
+        JasMain jasMain = new JasMain();
+        jasMain.run(files.toArray(new String[0]));
+        buildProgramProcess(testCase.params);
+    }
 
-            for (var record : compileVisitor.getRecords()) {
-                String fileName = record.name + ".j";
-                try (FileWriter fileWriter = new FileWriter(fileName)) {
-                    fileWriter.write(record.code);
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-                files.add(fileName);
-            }
-            JasMain jasMain = new JasMain();
-            jasMain.run(files.toArray(new String[0]));
-            buildProgramProcess(testCase.params);
+    public void run(TestCase testCase) throws InterruptedException, IOException {
+        try {
+            runUnsafe(testCase);
             System.out.println(ANSI_GREEN + "PASSED" + ANSI_RESET);
         } catch (Exception e) {
             Thread.sleep(500);
